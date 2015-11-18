d013f39be0492ac1872cc2d50999386d83123b1b
trasolan <trasolan@gmail.com>
2015-11-18 15:48:59 +0000

    Primer commit

---
+++ database/migrations/2015_11_12_193944_create_libros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo', 100);
+            $table->string('subtitulo', 100);
+            $table->string('leyenda', 100);
+            $table->smallInteger('edicion')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('libros');
+    }
+}
